4b6ad1f834dd7cbbeda21917d65834beb26a0b10
keoinn <keoinn@gmail.com>
2025-05-25 23:11:14 +0800

    Fix API with HTTPS.

--- src/plugins/GEHC/requests/requests.js
+++ src/plugins/GEHC/requests/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_BASEURL = import.meta.env.VITE_API_BASEURL
-const API_BASEURL = 'http://api.clock-trace.synet-app.com/v1/gehc/reports/'
+const API_BASEURL = 'https://api.clock-trace.synet-app.com/v1/gehc/reports/'
 
 const requestInstance = axios.create({
   baseURL: API_BASEURL,
